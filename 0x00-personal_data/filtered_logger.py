78995baa388dd65922afabd7899b703cb39d16ed
ando <terbu102@gmail.com>
2024-02-08 14:59:07 +0300

    pycode fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+'''filtered_logger module'''
 import re
+
+
 def filter_datum(fields, redaction, message, separator):
+    '''returns the log message obfuscated'''
     for field in fields:
-        message = re.sub(rf'{field}=(.*?){separator}', f'{field}={redaction}{separator}', message)
+        message = re.sub(rf'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
