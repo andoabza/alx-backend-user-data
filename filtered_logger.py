14c969f3bb17641b6fd5f2d068042dcba4546db4
ando <terbu102@gmail.com>
2024-02-08 10:13:35 +0300

    first commit

---
+++ filtered_logger.py
@@ -0,0 +1,10 @@
+'''filtered log'''
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''filter'''
+    for field in fields:
+        message = re.sub(rf'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
