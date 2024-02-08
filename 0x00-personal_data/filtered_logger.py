ff58d7d8cd9c4d3b41bbd3b81f2dde39a73fb206
ando <terbu102@gmail.com>
2024-02-08 15:25:37 +0300

    super format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''filtered_logger module'''
 import re
+import logging
 from typing import List
 
 
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = None) -> None:
+        '''Constructor'''
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''filter values in incoming log records using filter_datum'''
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
