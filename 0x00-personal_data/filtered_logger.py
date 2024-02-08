5a740afae4c87fcc607fc980149e83be3e2f811e
ando <terbu102@gmail.com>
2024-02-08 14:49:34 +0300

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-'''filtered log'''
 import re
-
-
 def filter_datum(fields, redaction, message, separator):
-    '''filter'''
     for field in fields:
-        message = re.sub(rf'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
+        message = re.sub(rf'{field}=(.*?){separator}', f'{field}={redaction}{separator}', message)
     return message
