a5973fc1d3af554e898a34283a2e4e74ac2f9b84
ando <terbu102@gmail.com>
2024-02-08 15:10:43 +0300

    tyope

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''filtered_logger module'''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(rf'{field}=(.*?){separator}',
