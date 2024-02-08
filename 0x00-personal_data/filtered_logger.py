6dc1aa257c2600ca4dc23c4713b337f8a9637e63
ando <terbu102@gmail.com>
2024-02-08 15:06:02 +0300

    type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,8 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: list, redaction: str,
+                 message: list, separator: str) -> str:
     '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(rf'{field}=(.*?){separator}',
