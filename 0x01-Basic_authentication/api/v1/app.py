c48fbaf9ed73814f371e8a9eda71b2f49e1156bf
ando <terbu102@gmail.com>
2024-02-12 22:01:54 +0300

    authenticate

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -53,6 +53,7 @@ def before_request() -> str:
         if auth.current_user(request) is None:
             abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
