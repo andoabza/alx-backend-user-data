10065d64fcac3a7297fa4337b0814558eb590aee
ando <terbu102@gmail.com>
2024-02-12 20:58:52 +0300

    api

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+
+#!/usr/bin/env python3
+"""authentication"""
+from api.v1.app import app_views
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''auth class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''authentication require'''
+        if excluded_paths and path in excluded_paths:
+            return False
+        if excluded_paths and path:
+            for paths in excluded_paths:
+                if paths.endswith('/'):
+                    lan = len(paths) - 1
+                    paths = paths[:lan]
+                    if paths ==  path:
+                        return False                 
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        '''secure the api on request'''
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,14 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def authorize() -> str:
+    return abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    '''forbidden'''
+    return abort(403)
