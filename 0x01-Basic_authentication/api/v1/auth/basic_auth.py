6c51a2579e4babcb59440bb2a5d925f4aebf78f7
ando <terbu102@gmail.com>
2024-02-12 22:24:05 +0300

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''basic auth'''
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''extract base64'''
+        if authorization_header is None:
+            return None
+        if authorization_header is not str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header[5:]
+        return None
+    
--- 0x01-Basic_authentication/main.py
+++ 0x01-Basic_authentication/main.py
@@ -1,11 +1,3 @@
-from api.v1.auth.auth import Auth
-
-a = Auth()
-
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
+a = ''
+if a is not int:
+    print('hello')
