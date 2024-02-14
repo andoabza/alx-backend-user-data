d2ea7c9c229a7344fb7f6310949db8ece0ba51de
ando <terbu102@gmail.com>
2024-02-14 22:02:24 +0300

    pycode fix

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ Module of session views
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from os import getenv
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
@@ -17,8 +18,12 @@ def login() -> str:
     user = User.search({'email': email})
     if not user:
        return { "error": "no user found for this email" }, 404
-    if not User.is_valid_password(password):
-         return { "error": "wrong password" }, 401
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-    return
+    passw = user[0].is_valid_password(password)
+    if not passw:
+        return { "error": "wrong password" }, 401
+    from api.v1.app import auth
+    id  = user[0].to_json()['id']
+    sess_id = auth.create_session(id)
+    result = jsonify(user[0].to_json())
+    result.set_cookie(getenv('SESSION_NAME'), sess_id)
+    return result
