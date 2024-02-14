2edef2183de5f06e179704decca9154b2ad95978
ando <terbu102@gmail.com>
2024-02-14 15:22:14 +0300

    session id by userid

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,5 +15,12 @@ class SessionAuth(Auth):
         '''create session id'''
         if isinstance(user_id, str):
             id = str(uuid.uuid4())
-            self.user_id_by_session_id.update({id: user_id})
+            self.user_id_by_session_id[id] = user_id
             return id
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''return user id  on session id'''
+        if isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
+        return None
