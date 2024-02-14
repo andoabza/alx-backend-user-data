16d4e140b0de33460327251e4aeaa2a1e9289645
ando <terbu102@gmail.com>
2024-02-14 21:11:36 +0300

    pocole

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,7 +54,8 @@ def before_request() -> str:
     excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded):
-        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        if auth.authorization_header(
+                request) is None and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
