00e8846a140f51498d11dce79415d791244672d5
ando <terbu102@gmail.com>
2024-02-14 21:09:38 +0300

    request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,12 +54,10 @@ def before_request() -> str:
     excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded):
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        if auth.authorization_header(request):
-            return None
-        if auth.session_cookie(request):
-            abort(401)
         request.current_user = auth.current_user(request)
 
 
