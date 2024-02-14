6d1d90cbe11776400f3c6c60b871cd093478ef8e
ando <terbu102@gmail.com>
2024-02-14 21:13:59 +0300

    return none

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,7 +55,9 @@ def before_request() -> str:
                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded):
         if auth.authorization_header(
-                request) is None and auth.session_cookie(request):
+                request) is None and auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None:
+                return None
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
