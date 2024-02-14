ab6fc2f7ccdd87d851f4c839de108e3f6600381c
ando <terbu102@gmail.com>
2024-02-14 21:07:03 +0300

    change the code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,8 +54,6 @@ def before_request() -> str:
     excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded):
-        if auth.authorization_header(request) is None:
-            abort(401)
         if auth.current_user(request) is None:
             abort(403)
         if auth.authorization_header(request):
