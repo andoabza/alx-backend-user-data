fabae4e8e5486f8301529967a403366cb83870eb
ando <terbu102@gmail.com>
2024-02-15 22:50:31 +0300

    session

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.session_auth import SessionAuth
 from os import getenv
 from datetime import datetime, timedelta
 
+
 class SessionExpAuth(SessionAuth):
     '''session expiration'''
     def __init__(self):
@@ -13,19 +14,19 @@ class SessionExpAuth(SessionAuth):
         if isinstance(getenv('SESSION_DURATION'), int):
             self.session_duration = getenv('SESSION_DURATION')
         self.session_duration = 0
-    
+
     def create_session(self, user_id=None):
         '''create session'''
         id = super()
         if not id:
             return None
-        
+
         self.user_id_by_session_id.update({id: {
             'user_id': user_id,
             'created_at': datetime.now()
         }})
         return id
-    
+
     def user_id_for_session_id(self, session_id=None):
         '''using user id find session id'''
         if not session_id:
@@ -35,6 +36,7 @@ class SessionExpAuth(SessionAuth):
                 return self.user_id_by_session_id[session_id]['user_id']
         if 'created_at' not in self.user_id_by_session_id[session_id]:
             return None
-        if self.user_id_by_session_id['123']['created_at'] + timedelta(seconds=60) < datetime.datetime.now():
-            return None    
+        if self.user_id_by_session_id['123']['created_at'] + timedelta(
+                seconds=60) < datetime.datetime.now():
+            return None
         return self.user_id_by_session_id[session_id]['user_id']
