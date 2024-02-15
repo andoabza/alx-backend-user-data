d62ca8b20d4d9a7470c45704f60726cdc2b0c50f
ando <terbu102@gmail.com>
2024-02-15 21:08:06 +0300

    strict slashes

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,30 +9,31 @@ from os import getenv
 from api.v1.app import auth
 
 
-
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
     '''login implementation'''
     email = request.form.get('email')
-    password  = request.form.get('password')
+    password = request.form.get('password')
     if not email or len(email) == 0:
-        return { "error": "email missing" }, 400
+        return {"error": "email missing"}, 400
     if not password or len(password) == 0:
-        return { "error": "password missing" }, 400
+        return {"error": "password missing"}, 400
     user = User.search({'email': email})
     if not user:
-       return { "error": "no user found for this email" }, 404
+        return {"error": "no user found for this email"}, 404
     passw = user[0].is_valid_password(password)
     if not passw:
-        return { "error": "wrong password" }, 401
+        return {"error": "wrong password"}, 401
     from api.v1.app import auth
-    id  = user[0].to_json()['id']
+    id = user[0].to_json()['id']
     sess_id = auth.create_session(id)
     result = jsonify(user[0].to_json())
     result.set_cookie(getenv('SESSION_NAME'), sess_id)
     return result
-        
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout() -> str:
     '''log out implementation'''
     if auth.destroy_session(request):
