247eda69a55fbc221f621356ce8d61c44a5c1402
ando <terbu102@gmail.com>
2024-02-15 23:37:59 +0300

    session db

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,7 +26,8 @@ if getenv('AUTH_TYPE') == 'session_auth':
     auth = SessionAuth()
 if getenv('AUTH_TYPE') == 'session_exp_auth':
     auth = SessionExpAuth()
-
+if getenv('AUTH_TYPE') == 'session_db_auth':
+    auth = SessionDBAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""User Session.
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    '''user session model'''
+    def __init__(self, *args: list, **kwargs: dict):
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
