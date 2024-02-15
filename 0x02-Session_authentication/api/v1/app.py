d9cd58be576ce96e778a91cc27d238faf93d25fd
ando <terbu102@gmail.com>
2024-02-15 21:20:46 +0300

    app_views

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,6 +57,8 @@ def before_request() -> str:
         if auth.authorization_header(
                 request) is None and auth.session_cookie(request) is None:
             return None, abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
         request.current_user = auth.current_user(request)
 
 
