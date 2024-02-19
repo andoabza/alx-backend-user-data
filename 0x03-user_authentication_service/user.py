618dfb476e33d8fd46a8a851ce5b8e3783e30b08
ando <terbu102@gmail.com>
2024-02-19 12:52:53 +0300

    database add user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 class User(Base):
     '''user model'''
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
