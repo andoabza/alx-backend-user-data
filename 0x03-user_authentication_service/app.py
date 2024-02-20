0b2c6b97c56a74499922d88cfd3ec63a1d653fb7
ando <terbu102@gmail.com>
2024-02-20 21:52:58 +0300

    if na

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """basic flask APP"""
 from flask import Flask, jsonify, request
-from auth import Auth
 
 app = Flask(__name__)
-AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False, methods=['GET'])
@@ -13,5 +11,19 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', strict_slashes=False, methods=['POST'])
+def users() -> str:
+    '''register users'''
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
+    from auth import Auth
+    AUTH = Auth()
     app.run(host="0.0.0.0", port="5000")
