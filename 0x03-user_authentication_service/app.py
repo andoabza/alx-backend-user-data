cf4eb94588e0226ebe4da193dd2e4e25cf0579da
ando <terbu102@gmail.com>
2024-02-20 21:48:00 +0300

    app authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,17 +3,17 @@
 from flask import Flask, jsonify, request
 from auth import Auth
 
-AUTH = Auth()
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/', strict_slashes=False, methods=['GET'])
+@app.route('/', methods=['GET'])
 def index() -> str:
     '''index path'''
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', strict_slashes=False, methods=['POST'])
+@app.route('/users', methods=['POST'])
 def users() -> str:
     '''register users'''
     try:
