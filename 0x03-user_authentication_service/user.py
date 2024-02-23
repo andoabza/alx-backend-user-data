1d4d33776c386e3c311bc23f60d6e5edb0bd5d3d
ando <terbu102@gmail.com>
2024-02-23 09:28:49 +0300

    users fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-'''users database table'''
-from sqlalchemy.ext.declarative import declarative_base
+"""The `user` model's module.
+"""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
 
 class User(Base):
-    '''user model'''
-    __tablename__ = 'users'
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    """Represents a record from the `user` table.
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance
-        """
-        super().__init__(*args, **kwargs)
