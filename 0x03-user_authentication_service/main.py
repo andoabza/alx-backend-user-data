c499cce116f8e0fcd2c558c780138036dbb78da4
ando <terbu102@gmail.com>
2024-02-23 09:35:23 +0300

    main

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,11 +1,16 @@
-from auth import Auth
-import bcrypt
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
-if not auth._db.find_user_by(email=email):
-    auth.register_user(email, password)
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
 
-# print(auth.valid_login(email, "WrongPwd"))
 
-# print(auth.valid_login("unknown@email", password))
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
